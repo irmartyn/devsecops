c2a0100af1e8c3d49cba93e62b379c8f5d839858
irmartyn <irsan8888@mail.ru>
2026-09-21 01:21:51 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+// Простой серверный скрипт с уязвимостью
+const express = require('express');
+const app = express();
+
+app.get('/run', (req, res) => { => {
+    // ОПАСНОСТЬ: eval() исполняет любой ввод пользователя как код!
+    let userInput = req.query.code;
+    let result = eval(userInput); 
+    res.send(result);
+});
+
+app.listen(3000);
