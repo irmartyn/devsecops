93973aaa20acb60616ef2077cac9427bea0cb06e
irmartyn <irsan8888@mail.ru>
2026-09-21 01:35:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/run', (req, res) => { => {
+app.get('/run', (req, res) => {
     // ОПАСНОСТЬ: eval() исполняет любой ввод пользователя как код!
     let userInput = req.query.code;
     let result = eval(userInput); 
